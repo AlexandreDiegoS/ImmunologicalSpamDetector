796f537e82c87ca302bf6dbe0b62b5f1875d7e0a
Airton Soares da Silva Júnior <assj@ecomp.poli.br>
2016-05-22 17:36:12 -0300

    Evolução da implementação do sistema #2
    - Adição dos métodos faltantes (verifyEmail, verifyTerms e evaluateDetectors);
    - Adição do arquivo "acceptedDetectors.txt" que será o esponsável por armazenar os detectores que foram validados;
    - Adição no "setup.properties" de alguns novos campos (os comentário no arquivo já explicam o que cada um significa);
    - Adição das bases de spams e de emails válidos
    - Refatoração e mudança de local de alguns métodos e algumas variáveis;
    
    Obs.: verificar a eficácia das validações e o tempo das iterações, percebi uma lentidão muito grande para poucos detectores e também nos poucos tetes que fiz ele não conseguiu ainda identificar os spams da base.

--- ImmunologicalSpamDetector/src/monitor/Monitor.java
+++ ImmunologicalSpamDetector/src/monitor/Monitor.java
@@ -1,5 +1,96 @@
 package monitor;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Properties;
+
+import census.Census;
+import detector.Detector;
+import util.PreprocessEmail;
+import util.file.Email;
+import util.file.ProjectProperties;
+
 public class Monitor {
+	private List<Detector> detectors;
 	
+	public Monitor() {
+		try {
+			File acceptedDetectorsFile = new File("res/acceptedDetectors.txt");
+			BufferedReader bufferedReader = new BufferedReader(new FileReader(acceptedDetectorsFile));
+			List<String> lines = new ArrayList<String>();
+			List<Detector> detectors = new ArrayList<Detector>();
+			
+			while(bufferedReader.ready()){
+				lines.add(bufferedReader.readLine());
+			}
+			
+			for(String line : lines){
+				String[] keyWords = line.split("#");
+				List<String> keyWordsList = new ArrayList<String>();
+				
+				for(String keyWord : keyWords){
+					keyWordsList.add(keyWord);
+				}
+				
+				detectors.add(new Detector(keyWordsList));
+			}
+			
+			bufferedReader.close();
+			this.detectors = detectors;
+		} catch (IOException e) {
+			e.printStackTrace();
+		}
+	}
+
+	public Monitor(List<Detector> detectors) {
+		this.detectors = detectors;
+	}
+
+	public boolean verifyEmail(File email){
+		boolean isValid = true;
+		Properties projectProperties = ProjectProperties.getProperties();
+		String emailContent = PreprocessEmail.clearEmailContent((Email.readEmailContent(email)));
+		String[] contentWords = emailContent.split(" ");
+		float spamDetectionPercentageThreshold = Float.parseFloat(projectProperties.getProperty("spamDetectionPercentageThreshold"));
+		int spamDetectionThreshold = Math.round(spamDetectionPercentageThreshold * this.detectors.size());
+		
+		int detectorMatchCount = 0;
+		for(Detector detector : this.detectors){
+			int matchCount = 0;
+			for(String keyWord : detector.getKeywords()){
+				for(String word : contentWords){
+					if(Census.verifyTerms(keyWord, word)){
+						matchCount++;
+						break;
+					}
+				}
+			}
+			
+			if(matchCount == detector.getKeywords().size()){
+				detectorMatchCount++;
+			}
+		}
+		
+		if(detectorMatchCount >= spamDetectionThreshold){
+			isValid = false;
+		}
+		
+		return isValid;
+	}
+	
+	public List<File> verifyEmails(List<File> emails){
+		List<File> validEmails = new ArrayList<File>();
+		
+		for(File email : emails){
+			if(this.verifyEmail(email)){
+				validEmails.add(email);
+			}
+		}
+		
+		return validEmails;
+	}
 }
